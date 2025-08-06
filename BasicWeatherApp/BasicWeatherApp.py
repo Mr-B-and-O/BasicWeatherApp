2185d4829f415d1a09d23623394d68477f609eab
Mr-B-and-O <rapulengmolosti@gmail.com>
2025-08-06 13:04:37 +0200

    Text Edit

--- BasicWeatherApp/BasicWeatherApp.py
+++ BasicWeatherApp/BasicWeatherApp.py
@@ -7,8 +7,8 @@ they are in. So the program is going to have a simple window show up when it sta
 the graphics with tkinter functions. Next It is going to output text which will be edited by tkinter and
 it will ask the user what city they are in so it can give the the time and the temperature. They input
 it which then the program will send requests to the server and retrieve data through the requests API.
-Then it will present the data which would be the time, and the temperature for that city. Of coarse their
-will also be exeptions to prevent crashes and it will have an executable. Will be on a loop so users can keep
+Then it will present the data which would be the time, and the temperature for that city. Ofcourse there
+will also be exceptions to prevent crashes and it will have an executable. Will be on a loop so users can keep
 requesting. I'll be uploading this on Git Hub as a repository and will be updating it till its done!
 '''
 
